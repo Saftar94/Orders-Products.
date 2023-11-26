681b21e04d46c21418711ccb9940c8bd91b1c0fd
aliev.saftar94@gmail.com <56716472+Saftar94@users.noreply.github.com>
2023-11-26 10:46:33 +0100

    push
    push

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import { Header } from "./components/header/header";
 import { NavSideBar } from "./components/navsidebar/navSideBar";
-import React, { useEffect } from "react";
-import { io } from "socket.io-client";
 
 function App() {
-  useEffect(() => {
-    const socket = io("wss://your-socket-server-url.com");
-
-    socket.on("activeSessions", (data) => {
-      console.log("Active sessions:", data);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
   return (
     <>
       <Header />
